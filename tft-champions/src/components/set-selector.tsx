cebd80387ebfff9ada4e731cfa331175252b828a
MihaiCatalin120 <mihaicatalin449@gmail.com>
2025-02-28 22:32:36 +0200

    Init project

---
+++ tft-champions/src/components/set-selector.tsx
@@ -0,0 +1,21 @@
+export default function SetSelector() {
+    return (
+        <select>
+            <option value="1">Set 1</option>
+            <option value="2">Set 2</option>
+            <option value="3">Set 3</option>
+            <option value="4">Set 4</option>
+            <option value="5">Set 5</option>
+            <option value="6">Set 6</option>
+            <option value="7">Set 7</option>
+            <option value="8">Set 8</option>
+            <option value="9">Set 9</option>
+            <option value="10">Set 10</option>
+            <option value="11">Set 11</option>
+            <option value="12">Set 12</option>
+            <option value="13">Set 13</option>
+            <option value="14">Set 14</option>
+            <option value="15">Set 15</option>
+        </select>
+    );
+}
